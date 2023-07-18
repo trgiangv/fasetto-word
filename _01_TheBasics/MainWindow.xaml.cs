abc5703423b88bf1c9e125da5cd78d727de03a15
TruongGiangVu <truonggiangvu610@gmail.com>
2023-07-19 00:21:17 +0700

    finish 01_TheBasics

--- _01_TheBasics/MainWindow.xaml.cs
+++ _01_TheBasics/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace _01_TheBasics
         {
             InitializeComponent();
         }
+
+        private void BttApply_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The Description is : {this.TxtDescription.Text}", "Information");
+        }
+
+        private void BttReset_Click(object sender, RoutedEventArgs e)
+        {
+            this.CbDrill.IsChecked 
+            = this.CbAssembly.IsChecked 
+            = this.CbFold.IsChecked 
+            = this.CbLaser.IsChecked 
+            = this.CbLathe.IsChecked
+            = this.CbPlasma.IsChecked 
+            = this.CbPurchase.IsChecked 
+            = this.CbRoll.IsChecked 
+            = this.CbWeld.IsChecked
+            = this.CbSaw.IsChecked = false;
+        }
+
+        private void Checkbox_checked(object sender, RoutedEventArgs e)
+        {
+            this.TxtLength.Text += (string)((CheckBox)sender).Content;
+        }
+
+        private void Finish_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.TxtNote == null)
+                return;
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+            this.TxtNote.Text = (string)value.Content;
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Finish_OnSelectionChanged(this.CbbFinish, null);
+        }
+
+        private void SupplierName_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.TxtMass.Text = this.TxtSupplierName.Text;
+        }
     }
 }
