de19af5c12f07ec20326dfa9a8cdc944c4834ddf
Truong Giang Vu <truonggiangvu610@gmail.com>
2023-07-21 16:55:32 +0700

    update

---
+++ _03_ViewModelMVVM/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+namespace _03_ViewModelMVVM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructor
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var d = new DirectoryStructureViewModel();
+            var item1 = d.Items[0];
+            d.Items[0].ExpandCommand.Execute(null);
+        }
+
+        #endregion
+        
+    }
+}
