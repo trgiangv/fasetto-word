6e943306fb01854e5f0a05ddfc23137b1499ba8f
Truong Giang Vu <85680448+trgiangv@users.noreply.github.com>
2023-07-30 00:48:30 +0700

    test
    test

---
+++ WPF_Python/command.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace WPF_Python
+{
+    internal class command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            Application app = uiapp.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            var a = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MechanicalEquipment).WhereElementIsNotElementType().ToElements();
+            Options options = new Options();
+            var c = a[0].get_Geometry(options);
+            return Result.Succeeded;
+        }
+    }
+}
