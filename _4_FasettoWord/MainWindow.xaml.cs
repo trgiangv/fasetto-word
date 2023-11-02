8244a2972bbf2a55631346dcd6012174593e57dc
Vu Truong Giang <85680448+trgiangv@users.noreply.github.com>
2023-11-02 23:13:37 +0700

    Update Fasetto Word

--- _4_FasettoWord/MainWindow.xaml.cs
+++ _4_FasettoWord/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace _4_FasettoWord
+﻿
+
+namespace _4_FasettoWord
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +10,7 @@
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new WindowViewModel(this);
         }
     }
 }
